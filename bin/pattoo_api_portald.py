2f89cec5a16120600b952c9f9745c4d585e2ec27
Cargill Seiveright <microcargill@hotmail.com>
2020-08-10 13:15:11 -0500

    Initial portal daemon

---
+++ bin/pattoo_api_portald.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""Pattoo WSGI script.
+
+Serves as a Gunicorn WSGI entry point for pattoo-api
+
+"""
+
+# Standard libraries
+import sys
+import os
+
+# Try to create a working PYTHONPATH
+_BIN_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+_ROOT_DIRECTORY = os.path.abspath(os.path.join(_BIN_DIRECTORY, os.pardir))
+_EXPECTED = '{0}pattoo{0}bin'.format(os.sep)
+if _BIN_DIRECTORY.endswith(_EXPECTED) is True:
+    sys.path.append(_ROOT_DIRECTORY)
+    default_pip_dir = '/opt/pattoo-daemon/.python'
+    sys.path.append(default_pip_dir)
+else:
+    print('''This script is not installed in the "{0}" directory. Please fix.\
+'''.format(_EXPECTED))
+    sys.exit(2)
+
+# Pattoo libraries
+from pattoo_shared import log
+from pattoo_shared.agent import Agent, AgentCLI, AgentAPI
+from pattoo.constants import (
+    PATTOO_API_PORTAL_NAME, PATTOO_API_PORTAL_PROXY)
+from pattoo.configuration import ConfigPortal as Config
+from pattoo.api.portal import PATTOO_PORTAL
+from pattoo.db.db import connectivity
+
+
+def main():
+    """Main function to start the Gunicorn WSGI."""
+    # Initialize key variables
+    config = Config()
+
+    # Make sure we have a database
+    _ = connectivity()
+
+    # Create agent object for web_proxy
+    agent_gunicorn = Agent(PATTOO_API_PORTAL_PROXY, config=config)
+
+    # Create agent for daemon
+    config = Config()
+    agent_api = AgentAPI(
+        PATTOO_API_PORTAL_NAME,
+        PATTOO_API_PORTAL_PROXY,
+        PATTOO_PORTAL,
+        config=config)
+
+    # Do control (API first, Gunicorn second)
+    cli = AgentCLI()
+    cli.control(agent_api)
+    cli.control(agent_gunicorn)
+
+
+if __name__ == '__main__':
+    log.env()
+    main()
