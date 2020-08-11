ab987b7336ba1b7fd62ac264c49e71bb7650c189
Cargill Seiveright <microcargill@hotmail.com>
2020-08-11 15:24:02 -0500

    Initial working simple page

---
+++ pattoo/api/portal/theme/static/js/app.js
@@ -0,0 +1,7 @@
+window.onload = function (){
+    console.log("Hello World");
+    document.getElementById("test-button").addEventListener(
+        'click', function(){
+            console.log("Hi there")
+    })
+};
