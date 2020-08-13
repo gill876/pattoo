219e9ebe6692c8e53bed05fcee2cd3047920ddad
Cargill Seiveright <microcargill@hotmail.com>
2020-08-13 11:52:10 -0500

    Add very simple login

--- pattoo/api/portal/panel.py
+++ pattoo/api/portal/panel.py
@@ -11,6 +11,7 @@ import uuid
 # Flask imports
 from flask import Blueprint, request, abort, session, jsonify
 from flask import render_template
+from flask import  session
 
 # pattoo imports
 from pattoo_shared import log
@@ -18,7 +19,11 @@ from pattoo_shared.constants import CACHE_KEYS
 from pattoo_shared.configuration import ServerConfig as Config
 from pattoo.constants import PATTOO_API_AGENT_NAME
 from pattoo_shared.files import get_gnupg
+from pattoo.db import db
+from pattoo.db.models import User as UserModel
 
+# Import server resources
+from .forms import LoginForm
 
 # Define the PANEL global variable
 PANEL = Blueprint('PANEL', __name__)
@@ -36,4 +41,16 @@ def index():
         response (int): Response code
 
     """
-    return render_template('index.html')
+    # Prepare login form
+    loginF = LoginForm()
+
+    if request.method == 'POST':
+        username = (request.form['username']).encode()
+        password = (request.form['password']).encode()
+
+        with db.db_query(20156, close=False) as session:
+            user = session.query(UserModel).filter_by(
+                username=username).first()
+
+            print("User found")
+    return render_template('index.html', form=loginF)
