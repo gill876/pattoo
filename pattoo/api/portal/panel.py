476a9e7a931fa47899698120dd12b6ea7f49d1a7
Cargill Seiveright <microcargill@hotmail.com>
2020-08-31 02:24:36 -0500

    Add modal route for datapoints of an agent

--- pattoo/api/portal/panel.py
+++ pattoo/api/portal/panel.py
@@ -24,6 +24,8 @@ from pattoo_shared.files import get_gnupg
 from pattoo.db import db
 from pattoo.db.models import User as UserModel
 from pattoo.db.models import Agent as AgentModel
+from pattoo.db.models import DataPoint as DataPModel
+from pattoo.db.models import PairXlate as PairModel
 
 # Import server resources
 from .forms import LoginForm
@@ -182,6 +184,39 @@ def agents():
             }]
         response = {'data':{'agents': pp_agents, 'message': 'Query ran'}}
     return response    
+
+
+@PANEL.route('/api/modal', methods=['GET'])
+def modal():
+    """
+    """
+    if session.get('idx_user', None) is None:
+        response = {'data': {'message': 'Login first'}}
+        return response
+
+    # enable = request.args.get('enable', type=int)
+    idx_agent = request.args.get('idx_agent', type=int)
+    response = {'data':{'message': 'Query did not run'}}
+    if idx_agent is not None:
+        with db.db_query(20189, close=False) as db_session:
+            datapoints = db_session.query(
+                DataPModel, PairModel
+            ).filter(
+                PairModel.idx_pair_xlate == DataPModel.data_type
+            ).filter(
+                DataPModel.idx_agent == idx_agent
+            ).all()
+            pp_datapoints = []
+            for datapoint in datapoints:
+                pp_datapoints+= [{
+                    'idx_datapoint': datapoint[0].idx_datapoint,
+                    'ts_created': datapoint[0].ts_created,
+                    'enabled': datapoint[0].enabled,
+                    'translation': (datapoint[1].translation).decode(),
+                    'polling_interval': datapoint[0].polling_interval
+                }]
+            response = {'data':{'datapoints': pp_datapoints, 'message': 'Query ran'}}
+    return response
 
 
 @PANEL.route('/adduser', methods=['GET', 'POST'])
